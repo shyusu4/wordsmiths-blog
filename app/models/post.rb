17806feed06968f96a2e55631c4f24c1c9d44d1f
shyusu4 <shyusupova4@gmail.com>
2022-12-08 15:16:01 +0500

    Add after save to post

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,7 @@ class Post < ApplicationRecord
     belongs_to :author, class_name: 'User', foreign_key: 'author_id'
     has_many :comments, foreign_key: 'post_id'
     has_many :likes, foreign_key: 'post_id'
+    after_save :update_posts_counter
 
     def update_posts_counter
         post.increment!(:posts_counter)
