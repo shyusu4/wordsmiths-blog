4838171e5b1993ba30cdce661159dbb2dc82ed02
shyusu4 <shyusupova4@gmail.com>
2022-12-08 15:11:38 +0500

    Add method that updates posts counter

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,8 @@ class Post < ApplicationRecord
     belongs_to :author, class_name: 'User', foreign_key: 'author_id'
     has_many :comments, foreign_key: 'post_id'
     has_many :likes, foreign_key: 'post_id'
+
+    def update_posts_counter
+        post.increment!(:posts_counter)
+    end
 end
