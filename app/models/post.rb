e789837e0f755323bf936a9984231bd832924fa6
shyusu4 <shyusupova4@gmail.com>
2022-12-10 20:14:40 +0500

    Add validations

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,6 +4,10 @@ class Post < ApplicationRecord
   has_many :likes, foreign_key: 'post_id'
   after_save :update_posts_counter
 
+  validates :title, presence: true, length: { minimum: 1, maximum: 250 }
+  validates :comments_counter, presence: true, numericality: { greater_than_or_equal_to: 0 }
+  validates :likes_counter, presence: true, numericality: { greater_than_or_equal_to: 0 }
+
   def update_posts_counter
     post.increment!(:posts_counter)
   end
