60b4ad2090bc367f1826e6395f01e7cb4885d63c
shyusu4 <shyusupova4@gmail.com>
2022-12-20 15:25:58 +0500

    Seperate test cases

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -5,8 +5,16 @@ RSpec.describe 'Posts', type: :request do
     it 'should pass with status 200' do
         get '/users/120/posts'
         expect(response.status).to eql(200)
+    end
+
+    it 'should render correct template' do
+        get '/users/120/posts'
         expect(response).to render_template(:index)
+    end
+
+    it 'should include correct text' do
+        get '/users/120/posts'
         expect(response.body).to include('List of posts by given user')
-      end
+    end
   end
 end
