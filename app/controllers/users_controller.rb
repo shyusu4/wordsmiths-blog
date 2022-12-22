4aca2ffe40e9e5715d641036e3969df8a4785523
shyusu4 <shyusupova4@gmail.com>
2022-12-22 22:38:20 +0500

    Adjust styles and add create posts btn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,10 @@
 class UsersController < ApplicationController
   def index
     @users = User.all
-    @user = current_user
   end
 
   def show
     @user = User.find(params[:id])
+    @current_user = current_user.id == @user.id
   end
 end
