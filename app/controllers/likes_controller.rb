d25a58de9c3d712dcf9f153387a2473ea1d123ce
shyusu4 <shyusupova4@gmail.com>
2022-12-22 23:52:03 +0500

    Add liking functionality

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -2,4 +2,11 @@ class LikesController < ApplicationController
   def new
     @like = Like.new
   end
+
+  def create
+    @like = Like.new(post_id: params[:id], author: current_user)
+    if @like.save
+    redirect_to user_post_path(current_user.id, params[:id])
+  end
+end
 end
