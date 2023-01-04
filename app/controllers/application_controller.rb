116ce377bcf399a32e64fab4eeeafc60bd2faf54
Bateyjosue <josuebatey19@gmail.com>
2023-01-04 11:19:59 +0200

    update cofingure-permetted_params function  in application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,9 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[email name])
+    devise_parameter_sanitizer.permit(:account_update) do |u|
+      u.permit(:name, :bio, :email, :password, :password_confirmation, :current_password)
+    end
   end
 end
