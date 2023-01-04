9064e699fedd927bb19147cf0a678bece76c30f4
shyusu4 <shyusupova4@gmail.com>
2023-01-04 17:11:47 +0500

    Add cancan ability

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_01_04_114117) do
+ActiveRecord::Schema[7.0].define(version: 2023_01_04_115648) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -46,7 +46,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_04_114117) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
-    t.string "photo"
+    t.string "photo", default: "https://camo.githubusercontent.com/eb6a385e0a1f0f787d72c0b0e0275bc4516a261b96a749f1cd1aa4cb8736daba/68747470733a2f2f612e736c61636b2d656467652e636f6d2f64663130642f696d672f617661746172732f6176615f303032322d3531322e706e67"
     t.text "bio"
     t.integer "posts_counter", default: 0
     t.datetime "created_at", null: false
