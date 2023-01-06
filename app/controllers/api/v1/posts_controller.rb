1ddfdf8da27fbd7df3047b0d75ebec7f0eb356e8
shyusu4 <shyusupova4@gmail.com>
2023-01-06 19:31:01 +0500

    Fix linter errors

--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -1,18 +1,18 @@
 module Api
   module V1
     class UsersController < ApplicationController
-      def index 
+      def index
         @user = User.find(params[:author_id])
         @posts = Post.find_by(author: @user)
         render json: @posts, status: 200
       end
 
-      def create 
+      def create
         @post = Post.new(author: current_user, title: params[:title], text: params[:text])
         if @post.save
-          render json: {response: 'post created successfully'}, status: 200
+          render json: { response: 'post created successfully' }, status: 200
         else
-          render error: { error: 'Post not created'}, status: 400
+          render error: { error: 'Post not created' }, status: 400
         end
       end
     end
