2b293cb9484fba155340d44914253086e0e3ad32
shyusu4 <shyusupova4@gmail.com>
2023-01-07 12:34:00 +0500

    Login if user is authenticated

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
 
-  # setting the root path for the application
   devise_scope :user do
     authenticated :user do
-      root :to => 'users#index', as: :authenticated_root
+      root :to => 'users#show', as: :authenticated_root
     end
     unauthenticated :user do
       root :to => 'devise/registrations#new', as: :unauthenticated_root
